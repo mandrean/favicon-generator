6144e814468636ab43de68de9c42e71933ebd7dc
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2015-02-03 16:57:03 +0100

    Moved stuff to separate config file. Implemented metaparser & markup generator.

--- index.js
+++ index.js
@@ -11,106 +11,35 @@
 
   var fs = require('fs'),
       async = require('async'),
-      im = require('imagemagick');
+      im = require('imagemagick'),
+      metaparser = require('metaparser'),
+      jf = require('jsonfile'),
 
-  var faviconSettings = {
-    android: {
-      name: 'favicon',
-      sizes: [
-        { width: 36, height: 36 },
-        { width: 48, height: 48 },
-        { width: 72, height: 72 },
-        { width: 96, height: 96 },
-        { width: 144, height: 144 },
-        { width: 192, height: 192 }
-      ]
-    },
-    appleStartup: {
-      name: 'apple-touch-startup-image',
-      sizes: [
-        { width: 1536, height: 2008, media: { deviceWidth: 768, deviceHeight: 1024, orientation: 'portrait', pixelratio: 2 }},
-        { width: 2048, height: 1496, media: { deviceWidth: 768, deviceHeight: 1024, orientation: 'landscape', pixelratio: 2 }},
-        { width: 768, height: 1004, media: { deviceWidth: 768, deviceHeight: 1024, orientation: 'portrait', pixelratio: 1 }},
-        { width: 1024, height: 748, media: { deviceWidth: 768, deviceHeight: 1024, orientation: 'landscape', pixelratio: 1 }},
-        { width: 640, height: 1096, media: { deviceWidth: 320, deviceHeight: 568, pixelratio: 2 }},
-        { width: 640, height: 920, media: { deviceWidth: 320, deviceHeight: 480, pixelratio: 2 }},
-        { width: 320, height: 460, media: { deviceWidth: 320, deviceHeight: 480, pixelratio: 1 }}
-      ]
-    },
-    appleTouch: {
-      name: 'apple-touch-icon',
-      sizes: [
-        { width: 57, height: 57 }, // Non-Retina iPhone, ≤ iOS 6
-        { width: 60, height: 60 }, // Non-Retina iPhone, ≥ iOS 7
-        { width: 72, height: 72 }, // Non-Retina iPad, ≤ iOS 6
-        { width: 76, height: 76 }, // Non-Retina iPad, ≥ iOS 7
-        { width: 114, height: 114 }, // Retina iPhone, ≤ iOS 6
-        { width: 120, height: 120 }, // Retina iPhone, ≥ iOS 7
-        { width: 144, height: 144 }, // Retina iPad, ≤ iOS 6
-        { width: 152, height: 152 }, // Retina iPad, ≥ iOS 7
-        { width: 180, height: 180 }  // iPhone 6 Plus, ≥ iOS 8
-      ]
-    },
-    coast: {
-      name: 'coast',
-      sizes: [
-        { width: 228, height: 228 }
-      ]
-    },
-    favico: {
-      name: 'favicon',
-      sizes: [
-        { width: 48, height: 48 },
-        { width: 32, height: 32 },
-        { width: 16, height: 16 }
-      ]
-    },
-    favicon: {
-      name: 'favicon',
-      sizes: [
-        { width: 160, height: 160 },
-        { width: 96, height: 96 },
-        { width: 16, height: 16 },
-        { width: 32, height: 32 }
-      ]
-    },
-    firefox: {
-      name: 'firefox_app',
-      sizes: [
-        { width: 60, height: 60 },
-        { width: 128, height: 128 },
-        { width: 512, height: 512 }
-      ]
-    },
-    opengraph: {
-      name: 'open-graph',
-      sizes: [
-        { width: 1500, height: 1500 }
-      ]
-    },
-    windows: {
-      name: 'mstile',
-      sizes: [
-        { width: 70, height: 70 },
-        { width: 144, height: 144 },
-        { width: 150, height: 150 },
-        { width: 310, height: 150 },
-        { width: 310, height: 310 }
-      ]
-    },
-    yandex: {
-      name: 'yandex',
-      sizes: [
-        { width: 50, height: 50 }
-      ]
-    }
-  };
+      config = jf.readFileSync('./config.json');
+
 
   module.exports = function (params) {
 
+    function setDimensions(favicon) {
+      var margin = params.design.global.margin || '0',
+          x = favicon.width,
+          y = favicon.height;
+      favicon.dimensions = x+'x'+y;
+      if (margin.indexOf('%') > -1) {
+        margin = parseFloat(margin)/100.0;
+        favicon.dimensionsWithoutMargin = Math.round(x*(1-margin))+'x'+Math.round(y*(1-margin));
+      }
+      else {
+        favicon.dimensionsWithoutMargin = (x-(margin*2))+'x'+(y-(margin*2));
+      }
+      return favicon
+    }
+
     function generateIcon(favicon) {
+      // background
+      favicon.dest = params.dest + favicon.href;
       im.convert([
-        favicon.src,
+        params.src,
         '-filter', 'Lanczos',
         '-resize', (favicon.dimensionsWithoutMargin ? favicon.dimensionsWithoutMargin : favicon.dimensions),
         '-background', (favicon.background ? favicon.background : 'transparent'),
@@ -118,44 +47,106 @@
         '-gravity', 'center',
         '-extent', favicon.dimensions,
         favicon.dest], function(err) {
-          if (err) throw err;
         }
       );
     }
 
-    function calculateDimensions(favicon, margin) {
-      var margin = margin || '0',
-          x = favicon.width,
-          y = favicon.height,
-          dimensions = x+'x'+y,
-          dimensionsWithoutMargin;
-      if (params.design.ios.margin.indexOf('%') > -1) {
-        margin = parseFloat(params.design.ios.margin)/100.0;
-        dimensionsWithoutMargin = Math.round(x*(1-margin))+'x'+Math.round(y*(1-margin));
+    function createMarkup(file, add) {
+      var remove = [
+        'link[rel="shortcut icon"]',
+        'link[rel="icon"]',
+        'link[rel^="apple-touch-icon"]',
+        'link[rel="manifest"]',
+        'link[rel="yandex-tableau-widget"]',
+        'link[rel="apple-touch-startup-image"]',
+        'meta[name^="msapplication"]',
+        'meta[name="mobile-web-app-capable"]',
+        'meta[name="theme-color"]',
+        'meta[name="apple-mobile-web-app-capable"]',
+        'meta[property="og:image"]'
+      ];
+
+      metaparser({
+        source: file,
+        add: add,
+        remove: remove,
+        out: file,
+        callback: function (error, html) {
+          if (error) throw error;
+          console.log(error, html);
+        }
+      });
+    }
+
+    function generateMarkup(element) {
+      if (element.element) {
+        return '<'
+          +element.element
+          +(element.name ? ' name="'+element.name+'"' : '')
+          +(element.property ? ' property="'+element.property+'"' : '')
+          +(element.content ? ' content="'+element.content+'"' : '')
+          +(element.rel ? ' rel="'+element.rel+'"' : '')
+          +(element.type ? ' type="'+element.type+'"' : '')
+          +(element.width && element.height ? ' sizes="'+element.width+'x'+element.height+'"' : '')
+          +(element.href ? ' href="'+element.href+'"' : '')
+          +(element.media ? ' media="'
+            +(element.media.deviceWidth ? '(device-width: '+element.media.deviceWidth+')' : '')
+            +(element.media.deviceWidth && element.media.deviceHeight ? ' and ' : '')
+            +(element.media.deviceHeight ? '(device-height: '+element.media.deviceHeight+')' : '')
+            +(element.media.orientation ? ' and (orientation: '+element.media.orientation+')' : '')
+            +(element.media.pixelratio ? ' and (-webkit-device-pixel-ratio: '+element.media.pixelratio+')' : '')
+          +'"' : '')+'>';
       }
-      else
-        dimensionsWithoutMargin = (x-(margin*2))+'x'+(y-(margin*2));
-      return [dimensions,dimensionsWithoutMargin]
     }
 
     function main() {
-      async.each(Object.keys(faviconSettings), function(favicons, callback) {
-        async.each(faviconSettings[favicons].sizes, function(favicon, callback) {
-          var dimensions = calculateDimensions(favicon, params.design.ios.margin),
-              filename = faviconSettings[favicons].name+'-'+dimensions[0]+'.png',
-              faviconParams = {
-                src: params.src,
-                dest: params.dest + filename,
-                dimensions: dimensions[0],
-                dimensionsWithoutMargin: dimensions[1],
-                background: params.design.ios.background_color
-              };
-          generateIcon(faviconParams);
-        }, function (err) {
-          callback(err)
-        });
-      }, function (err) {
-        callback(err);
+
+      var markup = [];
+
+      async.each(config.markup.elements, function(element, callback) {
+        if (element.element) {
+          if (element.sizes && (element.icon !== 'ico')) { // Icons+markup with multiple sizes, ignore .ico
+
+            for (var i=0, len=element.sizes.length; i<len; i++) {
+              if (element.sizes[i].width && element.sizes[i].height) {
+                element.width = element.sizes[i].width;
+                element.height = element.sizes[i].height;
+                if (element.hrefTemplate) {
+                  element.href = element.hrefTemplate.replace(/\{\{size\}\}/, element.sizes[i].width+'x'+element.sizes[i].height);
+                }
+                markup.push(generateMarkup(element) + '\n');
+                element = setDimensions(element);
+                generateIcon(element)
+              } else {
+                console.log('Error parsing sizes');
+                break;
+              }
+            }
+
+          } else if (element.icon === 'ico') { // Special case for favicon.ico
+              // generateIcon(faviconParams);
+            markup.push(generateMarkup(element) + '\n');
+
+          } else { // Single-size case
+            if (element.icon) { 
+              // generateIcon(faviconParams);
+            }
+            markup.push(generateMarkup(element) + '\n');
+          }
+
+        } else if (element.icon) { // Icon-only case
+          // generateIcon(faviconParams);
+        }
+        
+        callback();
+      },
+      function(err){
+        if(err) {
+          console.log('Error');
+        } else {
+          console.log('Done');
+          createMarkup(params.html, markup);
+        }
       });
     }
 
--- test/test.js
+++ test/test.js
@@ -4,52 +4,36 @@ faviconsGenerator({
   src: './test/flame.png',
   dest: './test/',
   icons_path: '',
-  html: '',
+  html: './test/favicons.html',
   design: {
-    ios: {
+    global: {
       picture_aspect: 'background_and_margin',
       margin: '33%',
       background_color: '#333'
     },
-    windows: {
-      picture_aspect: 'no_change',
-      background_color: '#333'
-    },
+    ios: {},
+    windows: {},
     firefox_app: {
       picture_aspect: 'circle',
       keep_picture_in_circle: 'true',
       circle_inner_margin: '5',
-      background_color: '#333',
       app_name: 'favicon-generator',
       app_description: 'Favicons generator for Node.js using ImageMagick',
       developer_name: 'Sebastian Mandrean',
       developer_url: 'https://github.com/mandrean/favicon-generator'
     },
-    android_chrome: {
-      picture_aspect: 'shadow',
-      theme_color: '#333'
-    },
-    coast: {
-      picture_aspect: 'background_and_margin',
-      background_color: '#333',
-      margin: '12%'
-    },
+    android_chrome: {},
+    coast: {},
     desktop_browser: {},
-    open_graph: {
-      picture_aspect: 'background_and_margin',
-      background_color: '#333',
-      margin: '12%',
-      ratio: '1.91:1'
-    },
-    yandex_browser: {
-      background_color: '#333'
-    }
+    open_graph: {},
+    yandex_browser: {}
   },
   tags: {
     add: [],
-    remove: ['link[rel="favicons"]']
+    remove: []
   },
   settings: {
-    compression: '5'
+    filter: 'Lanczos',
+    optimize: 'true'
   }
 });
